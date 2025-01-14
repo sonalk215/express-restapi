ee4b90f30105bb80fea2f7a3dad8d81f29aad8d2
SonalKhare <sonalkhare.sk215@gmail.com>
2025-01-14 23:46:19 +1100

    app commit

--- app.js
+++ app.js
@@ -28,10 +28,8 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-// const MONGODB_URI =
-// 'mongodb+srv://rebelthyworld:Forgot%402692_@cluster0.mq49lnp.mongodb.net/messagesDb?retryWrites=true&w=majority&appName=Cluster0';
 const MONGODB_URI =
-  'mongodb+srv://<mongodb_user>:<mongodb_user_password>@cluster0.q4mse.mongodb.net/messagesDb?retryWrites=true&w=majority&appName=Cluster0';
+  'mongodb+srv://root:root@cluster0.q4mse.mongodb.net/messagesDb?retryWrites=true&w=majority&appName=Cluster0';
 
 const feedRoutes = require('./routes/feed');
 const authRoutes = require('./routes/auth');
